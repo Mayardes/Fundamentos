d087b763a7d40498309c53904d5f446c2bc9b73e
Mayardes Oliveira <mayardesoliveira@gmail.com>
2023-01-20 13:34:32 -0400

    Add: Laços de repetição

--- CharpFundamentos/CharpFundamentos/Program.cs
+++ CharpFundamentos/CharpFundamentos/Program.cs
@@ -16,4 +16,35 @@ const int IDADE_MINIMA = 0; //Correto, inicia com ZERO;
 Console.WriteLine(IDADE_MINIMA);
 
 byte meuByte = 127;
-sbyte meubyteNegativo = -127; 
+sbyte meubyteNegativo = -127;
+
+int? idade = null!;
+Console.WriteLine($"Idade: {idade}");
+
+int inteiro = int.Parse("100"); //É uma extensão dos tipos primitivos.
+
+int inteiro2 = Convert.ToInt32(100);
+
+//***
+int inteiro3 = 0;
+Console.WriteLine(inteiro3);
+
+float real = 25.5f;
+Console.WriteLine(real);
+
+real = inteiro3; //Conversão implícita.
+inteiro3 = (int)real; //Conversão Explícita.
+
+Console.WriteLine($"inteiro: {inteiro3}");
+int inteiro4 = int.Parse("12");
+Console.WriteLine($"Inteiro: {inteiro4}");
+
+//Operadores Aritimétidos.
+
+//Laços de repetição.
+Console.WriteLine("Laços de repetição");
+for(int i=0; i<5; i++) {
+    for(int j =0; j<2; j++)
+        Console.WriteLine($"i:{i} - j:{j}");
+}
+    
